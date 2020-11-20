0975e28a76e0bd5d092a8f60cf6b61f526349a8e
Travis Shields <travis.shields@gmail.com>
2020-11-20 08:57:11 -0500

    Solve product except self

---
+++ productExceptSelf.js
@@ -0,0 +1,39 @@
+/* https://leetcode.com/problems/product-of-array-except-self/ */
+
+/* 
+Given an array nums of n integers where n > 1,
+return an array output such that output[i] is
+equal to the product of all the elements of nums
+except nums[i].
+
+Input:  [1,2,3,4]
+Output: [24,12,8,6]
+Constraint: It's guaranteed that the product of the
+elements of any prefix or suffix of the array (including
+the whole array) fits in a 32 bit integer.
+
+Note: Please solve it without division and in O(n).
+
+Follow up:
+Could you solve it with constant space complexity?
+(The output array does not count as extra space for
+the purpose of space complexity analysis.)
+*/
+
+const productExceptSelf = nums => {
+  const productsExceptIndex = [];
+
+  let productSoFar = 1;
+  for (let i = 0; i < nums.length; i++) {
+    productsExceptIndex[i] = productSoFar;
+    productSoFar *= nums[i];
+  }
+
+  productSoFar = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    productsExceptIndex[i] *= productSoFar;
+    productSoFar *= nums[i];
+  }
+
+  return productsExceptIndex;
+};
