3bd82d7b28d0763cef86af3eb771a0d515abe3fc
Travis Shields <travis.shields@gmail.com>
2023-06-27 13:53:47 -0400

    Update solution for maxSubArray & rob

--- js/maxSubArray.js
+++ js/maxSubArray.js
@@ -4,41 +4,31 @@
 Given an integer array nums, find the contiguous subarray (containing at least one number)
 which has the largest sum and return its sum.
 
-Follow up: If you have figured out the O(n) solution, try coding another solution using
-the divide and conquer approach, which is more subtle.
-
 Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
 Output: 6
-Explanation: [4,-1,2,1] has the largest sum = 6.
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+Example 2:
 
 Input: nums = [1]
 Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+Example 3:
 
-Input: nums = [0]
-Output: 0
-
-Input: nums = [-1]
-Output: -1
-
-Input: nums = [-2147483647]
-Output: -2147483647
+Input: nums = [5,4,-1,7,8]
+Output: 23
+Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
 */
 
-/* 
-[-2,1,-3,4,-1,2,1,-5,4]
-                     ^
-curr 5
-max  6
-*/
-
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 const maxSubArray = nums => {
-  let curr = 0;
-  let max = -Infinity;
-
-  nums.forEach(num => {
-    curr = Math.max(num, curr + num);
-    max = Math.max(max, curr);
-  });
-
-  return max;
+  let curSubArr = 0;
+  let maxSubArr = -Infinity;
+  for (let num of nums) {
+    curSubArr = Math.max(num, curSubArr + num);
+    maxSubArr = Math.max(maxSubArr, curSubArr);
+  }
+  return maxSubArr;
 };
--- js/rob.js
+++ js/rob.js
@@ -1,19 +1,34 @@
 /* https://leetcode.com/problems/house-robber/ */
 
+/*
+You are a professional robber planning to rob houses along a street.Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
 
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+Input: nums = [1, 2, 3, 1]
+Output: 4
+Explanation: Rob house 1(money = 1) and then rob house 3(money = 3).
+Total amount you can rob = 1 + 3 = 4.
+
+Input: nums = [2, 7, 9, 3, 1]
+Output: 12
+Explanation: Rob house 1(money = 2), rob house 3(money = 9) and rob house 5(money = 1).
+Total amount you can rob = 2 + 9 + 1 = 12.
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 const rob = nums => {
-  if (nums.length === 0) return 0;
-  if (nums.length === 1) return nums[0];
-
   let prevMax = 0;
-  let currentMax = 0;
+  let currMax = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    let newMax = Math.max(currentMax, prevMax + nums[i])
-    console.log({ num: nums[i], prevMax, currentMax, newMax})
-    prevMax = currentMax;
-    currentMax = newMax;
+  for (let num of nums) {
+    const newMax = Math.max(currMax, prevMax + num);
+    prevMax = currMax;
+    currMax = newMax;
   }
 
-  return currentMax;
+  return currMax;
 };
